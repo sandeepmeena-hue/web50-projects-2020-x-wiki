9d6636c5b2683ea5567659b55b82e13ff90ff4a7
sandeepmeena-hue <sandeepmeena-hue@users.noreply.github.com>
2020-04-29 04:19:06 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -181,10 +181,12 @@ int find_min(void)
 {
     int s=candidates[0].votes;
     for (int i = 0;i < candidate_count; i++)
-    {     if (s>candidates[i].votes)
-          {
-            s=candidates[i].votes;
-          }
+    {   if (candidates[i].eliminated==false)
+            {   if (s>candidates[i].votes)
+                {
+                    s=candidates[i].votes;
+                }
+            }
     }
     return s;
 }
