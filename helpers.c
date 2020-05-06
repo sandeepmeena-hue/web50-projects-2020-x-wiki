fdc7b39c282c78ae8e2379c10fb5725e762e406c
sandeepmeena-hue <sandeepmeena-hue@users.noreply.github.com>
2020-05-06 12:45:34 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -66,7 +66,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {   for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
-        {
+        {   int n = i;
+            int m = j;
             if (((i == 0) || (i == height-1)) && ((j == 0) || (j == width-1)))
             {   int sumG = 0;
                 int sumR = 0;
@@ -261,21 +262,28 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
             else
-            {   unsigned int sumG = 0;
-                unsigned int sumR = 0;
-                unsigned int sumB = 0;
-                for (int s = i-1; s < i+2; s++)
+            {    int sumG = 0;
+                 int sumR = 0;
+                 int sumB = 0;
+                 int count = 0;
+                for (int s = n-1; s < n+2; s++)
                 {
-                    for (int k = j-1; k < j+2; j++)
-                    {
-                            sumG+= image[s][k].rgbtGreen;
-                            sumR+= image[s][k].rgbtRed;
-                            sumB+= image[s][k].rgbtBlue;
+                    for (int k = m-1; k < n+2; k++)
+                    {   if (((s < height) && (k < width)) && ((s > 0) && (k > 0)))
+                        {    sumG+= image[s][k].rgbtGreen;
+                             sumR+= image[s][k].rgbtRed;
+                             sumB+= image[s][k].rgbtBlue;
+                             count+= 1;
+                        }
+                        else
+                        {
+                            continue;
+                        }
                     }
                 }
-                image[i][j].rgbtGreen = round ( (float) sumG/9);
-                image[i][j].rgbtBlue = round ( (float) sumB/9);
-                image[i][j].rgbtRed = round ( (float) sumR/9);
+                image[i][j].rgbtGreen = round ( (float) sumG/count);
+                image[i][j].rgbtBlue = round ( (float) sumB/count);
+                image[i][j].rgbtRed = round ( (float) sumR/count);
             }
         }
     }
